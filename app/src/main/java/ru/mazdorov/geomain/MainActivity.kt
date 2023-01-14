9fd0093d77ac3382af6f24b295c700cd4d13bdd3
mazdorovis <mazdorov.is@gmail.com>
2023-01-14 11:46:56 +0600

    previewButton added. Buttons replaced to ImageButtons

--- app/src/main/java/ru/mazdorov/geomain/MainActivity.kt
+++ app/src/main/java/ru/mazdorov/geomain/MainActivity.kt
@@ -5,13 +5,15 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var previewButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        previewButton = findViewById(R.id.preview_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -42,10 +45,24 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
         nextButton.setOnClickListener{
-            currentIndex = (currentIndex + 1) % questionBank.size
+            nextAction()
+        }
+        previewButton.setOnClickListener{
+            currentIndex = if(currentIndex == 0)
+                questionBank.size - 1
+            else
+                currentIndex - 1
             updateQuestion()
         }
+        questionTextView.setOnClickListener{view: View ->
+            nextAction()
+        }
 
+        updateQuestion()
+    }
+
+    private fun nextAction() {
+        currentIndex = (currentIndex + 1) % questionBank.size
         updateQuestion()
     }
 
